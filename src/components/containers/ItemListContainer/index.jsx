0e8605c777d1a05bc9599f5fbeb66b194e362649
Damian Oliva <damian.oliva@hotmail.com>
2022-06-27 15:46:44 -0300

    subida ultimo archivo

--- src/components/containers/ItemListContainer/index.jsx
+++ src/components/containers/ItemListContainer/index.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import yerba from '../../../assets/yerba.jpeg'
+// import yerba from '../../../assets/yerba.jpeg'
 
 const ItemListContainer = ({greeting}) => {
     return (
         <div>
             <p>{greeting}</p>
-            <img src={yerba} alt="yerba" height='500' width='500' />
+            {/* <img src={yerba} alt="yerba" height='500' width='500' /> */}
         </div>
     )
 }
